c0a8e9380ebb02789d9346378b106907fb6f0955
SM <sergio.martinez.edo@gmail.com>
2025-01-04 05:46:05 +0100

    x-frame options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :allow_iframe
+
+  private
+
+  def allow_iframe
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM https://codebrisk.co'
+    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://localhost:3000'
+  end
 end
