298c3f82aeeb75a78e2fe1cb75a1b174e3593fbe
SM <sergio.martinez.edo@gmail.com>
2025-01-18 09:57:03 +0100

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,5 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :allow_iframe
-
-  private
-
-  def allow_iframe
-    response.headers['X-Frame-Options'] = 'ALLOW-FROM http://localhost:3000'
-  end
 end
  
